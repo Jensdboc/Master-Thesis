6672fb5041346deb506ed62e747c91ad08c75d91
jensdboc <jensdboc.debock@ugent.be>
2024-05-16 11:31:27 +0200

    Cleanup runGIFmodel

---
+++ gif/runGIFmodels/createImageDataset.py
@@ -0,0 +1,14 @@
+import utils
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-s", "--skipframes", type=int, default=1)
+args = parser.parse_args()
+
+path2data = "../data/"
+
+skipframes = args.skipframes
+
+ids, labels, groups, catgs = utils.get_vids(path2data)
+utils.split_dataset(ids, labels, groups, skipframes=skipframes)
--- gif/runGIFmodels/runImageGIFModel.py
+++ gif/runGIFmodels/runImageGIFModel.py
@@ -1,8 +1,4 @@
 # python /project_ghent/Master-Thesis/ownModelNotebooks/testImages/runImageModel.py -b 16 -s 2 -e 10 -lr 0.00001 -name small
-import os
-
-os.system("pip install transformers")
-
 import utils
 import argparse
 import numpy as np
@@ -10,8 +6,6 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
-from torchvision.models import vgg16, resnet50
-from transformers import AutoImageProcessor, AutoModelForImageClassification
 
 seed = 42
 
@@ -48,54 +42,64 @@ num_gpu = args.gpu
 
 AMOUNT_OF_FRAMES = 40
 
-name = f"GRUimage_b{batch_size}s{skipframes}e{num_epochs}lr{lr}is{INPUT_SIZE}hs{HIDDEN_SIZE}nl{NUM_LAYERS}{args.name}bb{backbone}"
+name = f"image_b{batch_size}s{skipframes}e{num_epochs}lr{lr}is{INPUT_SIZE}hs{HIDDEN_SIZE}nl{NUM_LAYERS}{args.name}bb{backbone}"
 
 print("Retrieving datasets and loaders...")
 
 train_ds, val_ds = utils.get_dataset(skipframes=args.skip, name=args.name)
 
-train_dl = DataLoader(train_ds, batch_size=batch_size, shuffle=True, worker_init_fn=np.random.seed(seed), num_workers=num_workers)
-val_dl = DataLoader(val_ds, batch_size=2*batch_size, shuffle=False, num_workers=num_workers)  
-# test_dl = DataLoader(test_ds, batch_size=2*batch_size, shuffle=False, num_workers=num_workers)
+train_dl = DataLoader(
+    train_ds,
+    batch_size=batch_size,
+    shuffle=True,
+    worker_init_fn=np.random.seed(seed),
+    num_workers=num_workers,
+)
+val_dl = DataLoader(
+    val_ds, batch_size=2 * batch_size, shuffle=False, num_workers=num_workers
+)
 
 print("Initializing parameters...")
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 if backbone == "Dima806":
-    model = utils.BackBoneModelDima806(input_size=INPUT_SIZE, 
-                                   hidden_size=HIDDEN_SIZE, 
-                                   num_layers=NUM_LAYERS,
-                                   amount_of_frames=AMOUNT_OF_FRAMES//skipframes
-                                  ).to(device)
+    model = utils.BackBoneModelDima806(
+        input_size=INPUT_SIZE,
+        hidden_size=HIDDEN_SIZE,
+        num_layers=NUM_LAYERS,
+        amount_of_frames=AMOUNT_OF_FRAMES // skipframes,
+    ).to(device)
 else:
-    model = utils.RecipeImageModel(input_size=INPUT_SIZE, 
-                               hidden_size=HIDDEN_SIZE, 
-                               num_layers=NUM_LAYERS, 
-                               amount_of_frames=AMOUNT_OF_FRAMES // skipframes,
-                               backbone = backbone
-                              ).to(device)
+    model = utils.RecipeImageModel(
+        input_size=INPUT_SIZE,
+        hidden_size=HIDDEN_SIZE,
+        num_layers=NUM_LAYERS,
+        amount_of_frames=AMOUNT_OF_FRAMES // skipframes,
+        backbone=backbone,
+    ).to(device)
 
-print(torch.cuda.device_count())
 if torch.cuda.device_count() > 1:
     model = nn.DataParallel(model)
 model = model.to(device)
 
-# criterion = nn.BCEWithLogitsLoss()
-criterion = nn.BCEWithLogitsLoss(reduction='mean', pos_weight=torch.tensor([pos_weight]).to(device))
+criterion = nn.BCEWithLogitsLoss(
+    reduction="mean", pos_weight=torch.tensor([pos_weight]).to(device)
+)
 optimizer = optim.Adam(model.parameters(), lr=lr)
 
-parameters = {"device": device, 
-              "epochs": num_epochs,
-              "trainloader": train_dl,
-              "valloader": val_dl,
-              # "testloader": test_dl,
-              "criterion": criterion,
-              "optimizer": optimizer,
-              "batch_size": batch_size,
-              "amount_of_frames": AMOUNT_OF_FRAMES // skipframes,
-              "categories":  utils.categories.keys(),
-              "decision": 0.5}
+parameters = {
+    "device": device,
+    "epochs": num_epochs,
+    "trainloader": train_dl,
+    "valloader": val_dl,
+    "criterion": criterion,
+    "optimizer": optimizer,
+    "batch_size": batch_size,
+    "amount_of_frames": AMOUNT_OF_FRAMES // skipframes,
+    "categories": utils.categories.keys(),
+    "decision": 0.5,
+}
 
 print("Training...")
 
@@ -111,4 +115,4 @@ utils.plot_roc_curve(model, parameters, name, mode="Validation", verbose=False)
 
 print("Saving model...")
 
-torch.save(model, f"/project_ghent/Master-Thesis/gif/models/image_{name}.pth")
+torch.save(model, f"../gif/models/image_{name}.pth")
